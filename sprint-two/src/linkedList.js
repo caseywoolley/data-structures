34fb6b30e7d689065e95ff33e9a587100b201b51
Casey Woolley <caseywoolley@gmail.com>
2015-10-09 11:57:31 -0500

    completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,46 @@
-var LinkedList = function(){
+var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(value) {
+    //to hold the old tail
+    var oldTail = this.tail;
+    this.tail = Node(value);
+
+    if (oldTail != null) {
+      oldTail.next = this.tail;
+    }
+    if (list.head === null) {
+      this.head = this.tail
+    }
+  }
+
+
+  list.removeHead = function() {
+    var originalHead = this.head;
+    delete this.head;
+    this.head = originalHead.next;
+    return originalHead.value
   };
 
-  list.removeHead = function(){
-  };
+  list.contains = function(target) {
+    var currNode = this.head
+    while (currNode !== null) {
+      if (currNode.value === target) {
 
-  list.contains = function(target){
+        return true
+      }
+      currNode = currNode.next;
+    }
+
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value){
+var Node = function(value) {
   var node = {};
 
   node.value = value;
