35fc18911a57879a0f669c89ab70889c35597f61
Casey Woolley <caseywoolley@gmail.com>
2015-10-09 23:20:28 -0500

    completed set and hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,18 +1,54 @@
-var HashTable = function(){
+var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v){
+// [  [[k,v], [k1,v1], [k2, v2]] , , ,[ [k, v ], [k, v] ] ,   ]
+
+HashTable.prototype.insert = function(k, v) {
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage.get(i)) {
+    var empty = [];
+    this._storage.set(i, empty);
+
+  }
+  var foundIndex;
+ var pairs = this._storage.get(i);
+  for (var j = 0; j < pairs.length; j++) {
+    if (pairs[j][0] === k) {
+    	foundIndex = j;
+    }
+  }
+
+  if (foundIndex !== undefined) {
+  	pairs[foundIndex][1] = v;
+  } else {
+  	pairs.push([ k, v ]);
+  }
+
 };
 
-HashTable.prototype.retrieve = function(k){
+HashTable.prototype.retrieve = function(k) {
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  var pairs = this._storage.get(i);
+  for (var i = 0; i < pairs.length; i++) {
+    if (pairs[i][0] === k) {
+      return pairs[i][1]
+    }
+  }
+  return null;
 };
 
-HashTable.prototype.remove = function(k){
+
+HashTable.prototype.remove = function(k) {
+  var i = getIndexBelowMaxForKey(k, this._limit);
+   var pairs = this._storage.get(i);
+  for (var j = 0; j < pairs.length; j++) {
+  	if(pairs[j][0]===k){
+  		pairs[j].splice(j,1);
+  	}
+  };
 
 };
 
